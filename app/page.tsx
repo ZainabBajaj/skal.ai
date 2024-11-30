fb387a508a4d848fc0b835062ecfa6990bc3343f
Adan Asim <asimadan@gmail.com>
2024-11-30 12:45:21 +0500

    Nit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <div className="flex gap-4 items-center flex-col sm:flex-row">
         This site is under-development. Stay tuned for more.
       </div>
-        <a href="https://cbx-prod.b-cdn.net/COLOURBOX5104710.jpg?width=800&height=800&quality=70" className="text-blue-500">Click here to donate</a> {/* Add text-blue-500 to make the link blue */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p> Thank you </p>
