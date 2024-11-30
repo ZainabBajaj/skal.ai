cddbdcf986ca3a38717f40149cdb49a29628f788
Adan Asim <asimadan@gmail.com>
2024-11-30 12:42:09 +0500

    nit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-2xl font-semibold">Hello and Welcome!</h1> {/* Add class to ensure it's styled */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <div className="flex gap-4 items-center flex-col sm:flex-row">
-        This site is developed to collect donations for a mentally ill small girl, named Zaaj, who hails from a small town of Poland named War Burton. She&apos;s 44 years old physically, owns a brain of a 7 year old kid, and needs to get cured immediately.
+        This site is under-development. Stay tuned for more.
       </div>
         <a href="https://cbx-prod.b-cdn.net/COLOURBOX5104710.jpg?width=800&height=800&quality=70" className="text-blue-500">Click here to donate</a> {/* Add text-blue-500 to make the link blue */}
       </main>
